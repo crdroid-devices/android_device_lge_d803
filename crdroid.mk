eab4a0b51d989cf825fc035577d198786be9eae8
firebird11 <hbgassel@gmail.com>
2015-06-29 08:45:40 +0200

    crdroidify

--- crdroid.mk
+++ crdroid.mk
@@ -2,18 +2,18 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common crdroid stuff.
+$(call inherit-product, vendor/crdroid/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/crdroid/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/d803/d803.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := d803
-PRODUCT_NAME := cm_d803
+PRODUCT_NAME := crdroid_d803
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-D803
 PRODUCT_MANUFACTURER := lge
